8cb11ea8f45de2a00408ca9649b4aa80b08b7e8d
itsmemarl <marlzandrooo@gmail.com>
2024-12-10 15:22:55 +0800

    Update FinalNo4.java

--- CC2 Final Challenges/FinalNo4.java
+++ CC2 Final Challenges/FinalNo4.java
@@ -16,6 +16,7 @@ class Song {
 class Playlist {
     private Song head = null; // Start of the playlist
     private Song current = null; // Currently playing song
+
     // Add a song to the playlist
     public void addSong(String title, String artist) {
         Song newSong = new Song(title, artist); // Create a new song
@@ -58,6 +59,40 @@ class Playlist {
             System.out.println("Playlist is empty!");
         }
     }
+    // Remove a song from the playlist
+    public void removeSong(String title) {
+        if (head == null) { // If the playlist is empty
+            System.out.println("Playlist is empty!");
+            return;
+        }
+
+        Song temp = head; // Start from the head
+        do {
+            if (temp.title.equals(title)) { // Find the song to remove
+                if (temp == head && temp.next == head) { 
+                    // If it's the only song in the playlist
+                    head = null;
+                    current = null;
+                } else {
+                    temp.prev.next = temp.next; // Update the previous song's next link
+                    temp.next.prev = temp.prev; // Update the next song's prev link
+
+                    if (temp == head) {
+                        head = temp.next; // Update head if head is removed
+                    }
+                    if (temp == current) {
+                        current = temp.next; // Update current if current is removed
+                    }
+                }
+                System.out.println("Removed: " + temp.title + " by " + temp.artist);
+                return; // Exit after removing the song
+            }
+            temp = temp.next; // Move to the next song
+        } while (temp != head);
+
+        System.out.println("Song not found: " + title);
+    }
+
     // Display all songs in the playlist
     public void displayPlaylist() {
         if (head == null) {
@@ -79,22 +114,22 @@ public class FinalNo4 {
         playlist.addSong("Song 1", "Artist 1");
         playlist.addSong("Song 2", "Artist 2");
         playlist.addSong("Song 3", "Artist 3");
-
         // Display the playlist
         System.out.println("Playlist:");
         playlist.displayPlaylist();
-
+        // Remove a song
+        System.out.println("\nRemoving Song 2:");
+        playlist.removeSong("Song 2");
+        // Display the playlist again
+        System.out.println("\nUpdated Playlist:");
+        playlist.displayPlaylist();
         // Play songs
         System.out.println("\nStart playing:");
         playlist.playCurrent();
-
         System.out.println("\nNext song:");
         playlist.next();
-
-        System.out.println("\nNext song:");
-        playlist.next();
-
         System.out.println("\nPrevious song:");
         playlist.previous();
     }
 }
+
